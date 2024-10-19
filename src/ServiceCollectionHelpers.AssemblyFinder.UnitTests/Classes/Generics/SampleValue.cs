98b29ff510b793edac4c49f65ba34400a660f4f5
Mathieu Mack <mathieu.mack@viseo.com>
2024-10-19 23:03:49 +0200

    Add more unit tests for generic class. Update packages dependencies

---
+++ src/ServiceCollectionHelpers.AssemblyFinder.UnitTests/Classes/Generics/SampleValue.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ServiceCollectionHelpers.AssemblyFinder.Attributes;
+
+namespace ServiceCollectionHelpers.AssemblyFinder.UnitTests.Classes.Generics;
+
+[ServiceRegister(Scope = ServiceLifetime.Scoped)]
+internal class SampleValue<T>
+{
+    public T Value { get; set; }
+}
