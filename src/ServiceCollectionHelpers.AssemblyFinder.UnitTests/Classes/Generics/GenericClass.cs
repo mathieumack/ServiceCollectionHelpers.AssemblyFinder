0e735e1661144b3437feaab7d5571a4c3107f6cf
MACK Mathieu <mathieu.mack@outlook.fr>
2024-10-19 23:31:06 +0200

    Add more unit tests for generic class. (#12)
    * Add more unit tests for generic class.
    * Update packages dependencies
    
    Close #11

---
+++ src/ServiceCollectionHelpers.AssemblyFinder.UnitTests/Classes/Generics/GenericClass.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ServiceCollectionHelpers.AssemblyFinder.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceCollectionHelpers.AssemblyFinder.UnitTests.Classes.Generics;
+
+[ServiceRegister(Scope = ServiceLifetime.Scoped)]
+internal class GenericClass<T>
+{
+    private readonly SampleValue<T> sampleValue;
+
+    public GenericClass(SampleValue<T> sampleValue)
+    {
+        this.sampleValue = sampleValue;
+    }
+
+    public void Set(T value)
+    {
+        sampleValue.Value = value;
+    }
+
+    public T Get()
+    {
+        return sampleValue.Value;
+    }
+}
---
+++ src/ServiceCollectionHelpers.AssemblyFinder.UnitTests/Tests/GenericTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ServiceCollectionHelpers.AssemblyFinder.UnitTests.Classes.Abstractions;
+using ServiceCollectionHelpers.AssemblyFinder.UnitTests.Classes.Generics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceCollectionHelpers.AssemblyFinder.UnitTests.Tests;
+
+[TestClass]
+public class GenericTests
+{
+    [TestMethod]
+    public void Register_ScopedGenericClass()
+    {
+        var config = new ConfigurationBuilder()
+            .AddJsonFile("appsettings1.json")
+            .Build();
+
+        IServiceCollection serviceCollection = new ServiceCollection();
+
+        serviceCollection.RegisterTypes(config);
+
+        var serviceProvder = serviceCollection.BuildServiceProvider();
+
+        var intService = serviceProvder.GetRequiredService<GenericClass<int>>();
+        var stringService = serviceProvder.GetRequiredService<GenericClass<string>>();
+
+        intService.Set(1);
+        stringService.Set("test");
+
+        var intServiceScoped = serviceProvder.GetRequiredService<GenericClass<int>>();
+        var stringServiceScoped = serviceProvder.GetRequiredService<GenericClass<string>>();
+
+        Assert.AreEqual(1, intService.Get());
+        Assert.AreEqual(1, intServiceScoped.Get());
+        Assert.AreEqual("test", stringService.Get());
+        Assert.AreEqual("test", stringServiceScoped.Get());
+
+
+        intService.Set(10);
+        Assert.AreEqual(10, intService.Get());
+        Assert.AreEqual(10, intServiceScoped.Get());
+    }
+}
