2b3192df58c695eafffe3ee4fa1e00bc995e6156
Mathieu MACK <mathieu.mack@outlook.fr>
2025-11-13 13:05:34 +0100

    Remove useless usings

--- src/ServiceCollectionHelpers.AssemblyFinder/ServiceCollectionByAttributeExtensions.cs
+++ src/ServiceCollectionHelpers.AssemblyFinder/ServiceCollectionByAttributeExtensions.cs
@@ -3,92 +3,89 @@ using ServiceCollectionHelpers.AssemblyFinder.Attributes;
 using System;
 using System.Reflection;
 using System.Linq;
-using System.Configuration;
-using System.Xml.Linq;
 using Microsoft.Extensions.Configuration;
 
-namespace ServiceCollectionHelpers.AssemblyFinder
+namespace ServiceCollectionHelpers.AssemblyFinder;
+
+public static class ServiceCollectionByAttributeExtensions
 {
-    public static class ServiceCollectionByAttributeExtensions
+    /// <summary>
+    /// Register all class or interfaces that have the attribute <see cref="ServiceRegisterAttribute"/> defined
+    /// </summary>
+    /// <param name="serviceCollection"></param>
+    /// <param name="configuration"></param>
+    /// <returns></returns>
+    public static IServiceCollection RegisterTypes(this IServiceCollection serviceCollection, IConfiguration configuration)
     {
-        /// <summary>
-        /// Register all class or interfaces that have the attribute <see cref="ServiceRegisterAttribute"/> defined
-        /// </summary>
-        /// <param name="serviceCollection"></param>
-        /// <param name="configuration"></param>
-        /// <returns></returns>
-        public static IServiceCollection RegisterTypes(this IServiceCollection serviceCollection, IConfiguration configuration)
+        var assemblies = ServiceCollectionExtensions.GetAppDomainAssemblies();
+
+        try
         {
-            var assemblies = ServiceCollectionExtensions.GetAppDomainAssemblies();
-
-            try
+            foreach (var a in assemblies)
             {
-                foreach (var a in assemblies)
+                foreach (var t in a.GetTypes())
                 {
-                    foreach (var t in a.GetTypes())
+                    var attribute = t.GetCustomAttribute(typeof(ServiceRegisterAttribute));
+                    if (!t.IsInterface)
                     {
-                        var attribute = t.GetCustomAttribute(typeof(ServiceRegisterAttribute));
-                        if (!t.IsInterface)
+                        // We have to register the class as it.
+                        if (t.IsClass && !t.IsAbstract && attribute != null)
                         {
-                            // We have to register the class as it.
-                            if (t.IsClass && !t.IsAbstract && attribute != null)
+                            var serviceRegisterAttribute = attribute as ServiceRegisterAttribute;
+
+                            // Test configuration key and format (if provided)
+                            if (!string.IsNullOrEmpty(serviceRegisterAttribute.ConfigurationKey))
                             {
-                                var serviceRegisterAttribute = attribute as ServiceRegisterAttribute;
-
-                                // Test configuration key and format (if provided)
-                                if (!string.IsNullOrEmpty(serviceRegisterAttribute.ConfigurationKey))
+                                if (string.IsNullOrEmpty(serviceRegisterAttribute.ConfigurationKeyFormat))
                                 {
-                                    if (string.IsNullOrEmpty(serviceRegisterAttribute.ConfigurationKeyFormat))
-                                    {
-                                        var variableValue = serviceRegisterAttribute.ConfigurationKey.GetAppSettingsValue(configuration);
-                                        if (string.IsNullOrEmpty(variableValue))
-                                            continue;
-                                    }
-                                    else
-                                    {
-                                        var variableValue = serviceRegisterAttribute.ConfigurationKey.GetAppSettingsValue(configuration);
-                                        if (string.IsNullOrEmpty(variableValue))
-                                            continue;
-
-                                        var regex = new System.Text.RegularExpressions.Regex(serviceRegisterAttribute.ConfigurationKeyFormat, System.Text.RegularExpressions.RegexOptions.IgnoreCase, new TimeSpan(0,0,5));
-                                        if (!regex.IsMatch(variableValue))
-                                            continue;
-                                    }
+                                    var variableValue = serviceRegisterAttribute.ConfigurationKey.GetAppSettingsValue(configuration);
+                                    if (string.IsNullOrEmpty(variableValue))
+                                        continue;
                                 }
-
-                                var registeroption = new RegisterAsOptions()
-                                {
-                                    ServiceLifetime = serviceRegisterAttribute.Scope
-                                };
-
-                                if (!serviceRegisterAttribute.RegisterWithInterface)
-                                    serviceCollection.Register(t, t, registeroption);
                                 else
                                 {
-                                    foreach(var interfaceType in t.GetInterfaces())
-                                    {
-                                        serviceCollection.Register(t, interfaceType, registeroption);
-                                    }
+                                    var variableValue = serviceRegisterAttribute.ConfigurationKey.GetAppSettingsValue(configuration);
+                                    if (string.IsNullOrEmpty(variableValue))
+                                        continue;
+
+                                    var regex = new System.Text.RegularExpressions.Regex(serviceRegisterAttribute.ConfigurationKeyFormat, System.Text.RegularExpressions.RegexOptions.IgnoreCase, new TimeSpan(0,0,5));
+                                    if (!regex.IsMatch(variableValue))
+                                        continue;
+                                }
+                            }
+
+                            var registeroption = new RegisterAsOptions()
+                            {
+                                ServiceLifetime = serviceRegisterAttribute.Scope
+                            };
+
+                            if (!serviceRegisterAttribute.RegisterWithInterface)
+                                serviceCollection.Register(t, t, registeroption);
+                            else
+                            {
+                                foreach(var interfaceType in t.GetInterfaces())
+                                {
+                                    serviceCollection.Register(t, interfaceType, registeroption);
                                 }
                             }
                         }
                     }
                 }
             }
-            catch (ReflectionTypeLoadException ex)
-            {
-                var msg = string.Join(Environment.NewLine, ex.LoaderExceptions.Select(e => e.Message));
-                var fail = new Exception(msg, ex);
-
-                throw fail;
-            }
-
-            return serviceCollection;
         }
-
-        private static string GetAppSettingsValue(this string variableKey, IConfiguration configuration)
+        catch (ReflectionTypeLoadException ex)
         {
-            return configuration.GetSection(variableKey).Value;
+            var msg = string.Join(Environment.NewLine, ex.LoaderExceptions.Select(e => e.Message));
+            var fail = new Exception(msg, ex);
+
+            throw fail;
         }
+
+        return serviceCollection;
+    }
+
+    private static string GetAppSettingsValue(this string variableKey, IConfiguration configuration)
+    {
+        return configuration.GetSection(variableKey).Value;
     }
 }
--- src/ServiceCollectionHelpers.AssemblyFinder/ServiceCollectionExtensions.cs
+++ src/ServiceCollectionHelpers.AssemblyFinder/ServiceCollectionExtensions.cs
@@ -5,167 +5,166 @@ using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
 
-namespace ServiceCollectionHelpers.AssemblyFinder
+namespace ServiceCollectionHelpers.AssemblyFinder;
+
+public static class ServiceCollectionExtensions
 {
-    public static class ServiceCollectionExtensions
+    public static IServiceCollection RegisterTypes<T>(this IServiceCollection serviceCollection)
     {
-        public static IServiceCollection RegisterTypes<T>(this IServiceCollection serviceCollection)
+        return serviceCollection.RegisterClassesOfType(typeof(T), new RegisterAsOptions()
         {
-            return serviceCollection.RegisterClassesOfType(typeof(T), new RegisterAsOptions()
-            {
-                ServiceLifetime = ServiceLifetime.Scoped
-            });
-        }
-
-        public static IServiceCollection RegisterClassesOfType<T>(this IServiceCollection serviceCollection, RegisterAsOptions options = null)
-        {
-            return serviceCollection.RegisterClassesOfType(typeof(T), options ?? new RegisterAsOptions()
-            {
-                ServiceLifetime = ServiceLifetime.Scoped
-            });
-        }
-
-        public static IServiceCollection RegisterClassesOfType(this IServiceCollection serviceCollection, Type assignTypeFrom, RegisterAsOptions options = null)
-        {
-            // Add default assemblies :
-            if(!options.Assemblies.Any())
-                options.Assemblies.AddRange(GetAppDomainAssemblies());
-
-            if(assignTypeFrom.IsInterface)
-            {
-                // We have to find all classes that implements this one
-                return serviceCollection.RegisterClassesThatInheritsInterface(assignTypeFrom, options);
-            }
-            else
-            {
-                // We have to find all classes that inherits from this one
-                return serviceCollection.RegisterClassesThatInheritsFrom(assignTypeFrom, options);
-            }
-        }
-
-        private static IServiceCollection RegisterClassesThatInheritsInterface(this IServiceCollection serviceCollection, Type interfaceType, RegisterAsOptions options = null)
-        {
-            try
-            {
-                foreach (var a in options.Assemblies)
-                {
-                    foreach (var t in a.GetTypes())
-                    {
-                        if (!t.IsInterface && t.GetInterfaces().Contains(interfaceType))
-                        {
-                            if (t.IsClass && !t.IsAbstract)
-                            {
-                                serviceCollection.Register(t, interfaceType, options);
-                            }
-                        }
-                    }
-                }
-            }
-            catch (ReflectionTypeLoadException ex)
-            {
-                var msg = string.Join(Environment.NewLine, ex.LoaderExceptions.Select(e => e.Message));
-
-                var fail = new Exception(msg, ex);
-
-                throw fail;
-            }
-
-
-            return serviceCollection;
-        }
-
-        internal static IServiceCollection RegisterClassesThatInheritsFrom(this IServiceCollection serviceCollection, Type inheritsType, RegisterAsOptions options = null)
-        {
-            var result = new List<Type>();
-            try
-            {
-                foreach (var a in options.Assemblies)
-                {
-                    foreach (var t in a.GetTypes())
-                    {
-                        if (!t.IsInterface && t.GetNestedTypes().Contains(inheritsType))
-                        {
-                            if (t.IsClass && !t.IsAbstract)
-                            {
-                                result.Add(t);
-                            }
-                        }
-                    }
-                }
-            }
-            catch (ReflectionTypeLoadException ex)
-            {
-                var msg = string.Join(Environment.NewLine, ex.LoaderExceptions.Select(e => e.Message));
-
-                var fail = new Exception(msg, ex);
-                Debug.WriteLine(fail.Message, fail);
-
-                throw fail;
-            }
-
-            serviceCollection.Register(result, inheritsType, options);
-
-            return serviceCollection;
-        }
-
-        internal static void Register(this IServiceCollection serviceCollection, List<Type> types, Type serviceType, RegisterAsOptions options)
-        {
-            foreach (var type in types)
-            {
-                serviceCollection.Register(type, serviceType, options);
-            }
-        }
-
-        internal static void Register(this IServiceCollection serviceCollection, Type type, Type serviceType, RegisterAsOptions options)
-        {
-            if (options.ServiceLifetime == ServiceLifetime.Transient)
-                serviceCollection.AddTransient(serviceType, type);
-            else if (options.ServiceLifetime == ServiceLifetime.Scoped)
-                serviceCollection.AddScoped(serviceType, type);
-            else if (options.ServiceLifetime == ServiceLifetime.Singleton)
-                serviceCollection.AddSingleton(serviceType, type);
-            else
-                throw new InvalidOperationException("The RegisterAs option is not recongnised as a valid type. Supported :Transient, Scoped, Singleton.");
-        }
-
-        /// <summary>
-        /// Gets tne assemblies related to the current implementation.
-        /// </summary>
-        /// <returns>A list of assemblies that should be loaded by the Nop factory.</returns>
-        internal static IList<Assembly> GetAppDomainAssemblies()
-        {
-            return AppDomain.CurrentDomain.GetAssemblies().OrderBy(e => e.FullName).ToList();
-        }
-
-        #region Utilities
-
-        /// <summary>
-        /// Does type implement generic?
-        /// </summary>
-        /// <param name="type"></param>
-        /// <param name="openGeneric"></param>
-        /// <returns></returns>
-        internal static bool DoesTypeImplementOpenGeneric(Type type, Type openGeneric)
-        {
-            try
-            {
-                var genericTypeDefinition = openGeneric.GetGenericTypeDefinition();
-                foreach (var implementedInterface in type.FindInterfaces((objType, objCriteria) => true, null))
-                {
-                    if (!implementedInterface.IsGenericType)
-                        continue;
-
-                    var isMatch = genericTypeDefinition.IsAssignableFrom(implementedInterface.GetGenericTypeDefinition());
-                    return isMatch;
-                }
-                return false;
-            }
-            catch
-            {
-                return false;
-            }
-        }
-
-        #endregion
+            ServiceLifetime = ServiceLifetime.Scoped
+        });
     }
+
+    public static IServiceCollection RegisterClassesOfType<T>(this IServiceCollection serviceCollection, RegisterAsOptions options = null)
+    {
+        return serviceCollection.RegisterClassesOfType(typeof(T), options ?? new RegisterAsOptions()
+        {
+            ServiceLifetime = ServiceLifetime.Scoped
+        });
+    }
+
+    public static IServiceCollection RegisterClassesOfType(this IServiceCollection serviceCollection, Type assignTypeFrom, RegisterAsOptions options = null)
+    {
+        // Add default assemblies :
+        if(!options.Assemblies.Any())
+            options.Assemblies.AddRange(GetAppDomainAssemblies());
+
+        if(assignTypeFrom.IsInterface)
+        {
+            // We have to find all classes that implements this one
+            return serviceCollection.RegisterClassesThatInheritsInterface(assignTypeFrom, options);
+        }
+        else
+        {
+            // We have to find all classes that inherits from this one
+            return serviceCollection.RegisterClassesThatInheritsFrom(assignTypeFrom, options);
+        }
+    }
+
+    private static IServiceCollection RegisterClassesThatInheritsInterface(this IServiceCollection serviceCollection, Type interfaceType, RegisterAsOptions options = null)
+    {
+        try
+        {
+            foreach (var a in options.Assemblies)
+            {
+                foreach (var t in a.GetTypes())
+                {
+                    if (!t.IsInterface && t.GetInterfaces().Contains(interfaceType))
+                    {
+                        if (t.IsClass && !t.IsAbstract)
+                        {
+                            serviceCollection.Register(t, interfaceType, options);
+                        }
+                    }
+                }
+            }
+        }
+        catch (ReflectionTypeLoadException ex)
+        {
+            var msg = string.Join(Environment.NewLine, ex.LoaderExceptions.Select(e => e.Message));
+
+            var fail = new Exception(msg, ex);
+
+            throw fail;
+        }
+
+
+        return serviceCollection;
+    }
+
+    internal static IServiceCollection RegisterClassesThatInheritsFrom(this IServiceCollection serviceCollection, Type inheritsType, RegisterAsOptions options = null)
+    {
+        var result = new List<Type>();
+        try
+        {
+            foreach (var a in options.Assemblies)
+            {
+                foreach (var t in a.GetTypes())
+                {
+                    if (!t.IsInterface && t.GetNestedTypes().Contains(inheritsType))
+                    {
+                        if (t.IsClass && !t.IsAbstract)
+                        {
+                            result.Add(t);
+                        }
+                    }
+                }
+            }
+        }
+        catch (ReflectionTypeLoadException ex)
+        {
+            var msg = string.Join(Environment.NewLine, ex.LoaderExceptions.Select(e => e.Message));
+
+            var fail = new Exception(msg, ex);
+            Debug.WriteLine(fail.Message, fail);
+
+            throw fail;
+        }
+
+        serviceCollection.Register(result, inheritsType, options);
+
+        return serviceCollection;
+    }
+
+    internal static void Register(this IServiceCollection serviceCollection, List<Type> types, Type serviceType, RegisterAsOptions options)
+    {
+        foreach (var type in types)
+        {
+            serviceCollection.Register(type, serviceType, options);
+        }
+    }
+
+    internal static void Register(this IServiceCollection serviceCollection, Type type, Type serviceType, RegisterAsOptions options)
+    {
+        if (options.ServiceLifetime == ServiceLifetime.Transient)
+            serviceCollection.AddTransient(serviceType, type);
+        else if (options.ServiceLifetime == ServiceLifetime.Scoped)
+            serviceCollection.AddScoped(serviceType, type);
+        else if (options.ServiceLifetime == ServiceLifetime.Singleton)
+            serviceCollection.AddSingleton(serviceType, type);
+        else
+            throw new InvalidOperationException("The RegisterAs option is not recongnised as a valid type. Supported :Transient, Scoped, Singleton.");
+    }
+
+    /// <summary>
+    /// Gets tne assemblies related to the current implementation.
+    /// </summary>
+    /// <returns>A list of assemblies that should be loaded by the Nop factory.</returns>
+    internal static IList<Assembly> GetAppDomainAssemblies()
+    {
+        return AppDomain.CurrentDomain.GetAssemblies().OrderBy(e => e.FullName).ToList();
+    }
+
+    #region Utilities
+
+    /// <summary>
+    /// Does type implement generic?
+    /// </summary>
+    /// <param name="type"></param>
+    /// <param name="openGeneric"></param>
+    /// <returns></returns>
+    internal static bool DoesTypeImplementOpenGeneric(Type type, Type openGeneric)
+    {
+        try
+        {
+            var genericTypeDefinition = openGeneric.GetGenericTypeDefinition();
+            foreach (var implementedInterface in type.FindInterfaces((objType, objCriteria) => true, null))
+            {
+                if (!implementedInterface.IsGenericType)
+                    continue;
+
+                var isMatch = genericTypeDefinition.IsAssignableFrom(implementedInterface.GetGenericTypeDefinition());
+                return isMatch;
+            }
+            return false;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+    #endregion
 }
