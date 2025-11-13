df10066161a74cdc7f1272066ada85ec41735d76
MACK Mathieu <mathieu.mack@outlook.fr>
2025-11-13 13:11:01 +0100

    Update project to target .NET 10.0 and upgrade dependencies (#21)
    * Update project to target .NET 10.0 and upgrade package references to latest versions
    
    Related to #20
    
    * Conditional Package References by Target Framework
    
    Updated `ServiceCollectionHelpers.AssemblyFinder.csproj` to conditionally include package references based on the target framework version. For `net9.0`, use version `9.0.11` of `Microsoft.Extensions.Configuration.Abstractions` and `Microsoft.Extensions.DependencyInjection.Abstractions`. For `net10.0`, use version `10.0.0`. This change enhances flexibility and ensures compatibility with different .NET framework versions.

--- src/ServiceCollectionHelpers.AssemblyFinder/RegisterAsOptions.cs
+++ src/ServiceCollectionHelpers.AssemblyFinder/RegisterAsOptions.cs
@@ -4,20 +4,19 @@ using System.Collections.Generic;
 using System.Reflection;
 using System.Text;
 
-namespace ServiceCollectionHelpers.AssemblyFinder
+namespace ServiceCollectionHelpers.AssemblyFinder;
+
+public class RegisterAsOptions
 {
-    public class RegisterAsOptions
-    {
-        /// <summary>
-        /// Indicate the type of register in the service collection
-        /// </summary>
-        public ServiceLifetime ServiceLifetime { get; set; } = ServiceLifetime.Transient;
+    /// <summary>
+    /// Indicate the type of register in the service collection
+    /// </summary>
+    public ServiceLifetime ServiceLifetime { get; set; } = ServiceLifetime.Transient;
 
-        ///// <summary>
-        ///// Limit register to only concrete class, not abstract
-        ///// </summary>
-        //public bool RegisterOnlyConcreteClass { get; set; } = true;
+    ///// <summary>
+    ///// Limit register to only concrete class, not abstract
+    ///// </summary>
+    //public bool RegisterOnlyConcreteClass { get; set; } = true;
 
-        public List<Assembly> Assemblies { get; } = new List<Assembly>();
-    }
+    public List<Assembly> Assemblies { get; } = new List<Assembly>();
 }
